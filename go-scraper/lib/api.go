f3d2449519f59c4583b3c7078799ee060ff723e9
alpakathrowaway <games.vincent@gmx.de>
2021-10-09 19:59:19 +0200

    added API struct

--- go-scraper/lib/api.go
+++ go-scraper/lib/api.go
@@ -1,1 +1,23 @@
+// The api calls to the backend server
 package goscraper_lib
+
+import (
+	"net/http"
+)
+
+// API struct holds all necessary information
+type API struct {
+	server string
+	port   int
+	client *http.Client
+}
+
+// InitAPI initialises and returns a new API struct
+func InitApi(server string, port int) *API {
+	api := API{
+		server: server,
+		port:   port,
+		client: http.DefaultClient,
+	}
+	return &api
+}
