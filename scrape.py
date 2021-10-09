e0d8613e68c53044525df8eafedef4ab9498cc4f
TestTest784 <56189374+TestTest784@users.noreply.github.com>
2021-10-10 00:25:47 +0200

    finished the scraper

--- scrape.py
+++ scrape.py
@@ -4,28 +4,32 @@ import requests
 from bs4 import BeautifulSoup
 import html_text
 import re
+def getruegen():
+    url: str = "https://www.presserat.de/ruegen-presse-uebersicht.html"
+    page = requests.get(url)
 
-url: str = "https://www.presserat.de/ruegen-presse-uebersicht.html"
-page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    main = soup.find('main')
 
-soup = BeautifulSoup(page.text, 'html.parser')
-main = soup.find('main')
-
-for section in main.find_all('section'):
-    div1, div2 = section.find_all('div', recursive=False)
-    year = div1.text.strip()
-    txt = html_text.extract_text(str(div2))
-    for line in txt.splitlines():
-        if "(" in line:
-            medium, line = line.split('(', 1)
-            az, line = re.split("[,\)]", line, 1)
-            if '"' in line or "'" in line:
-                line = line.lstrip(": ")
-                title,line = line.split (",",1)
-            else:
-                title = ""
-            line = line.lstrip (", ")
-            iffer = line.split(",") [0]
-            #temp, line = re.split('[\'"]', line, 1)
-                #titel, line = re.split('[\'"]', line, 1)
-            print(medium,az,title,iffer)
+    ruegenliste = []
+    for section in main.find_all('section'):
+        div1, div2 = section.find_all('div', recursive=False)
+        year = div1.text.strip()
+        txt = html_text.extract_text(str(div2))
+        for line in txt.splitlines():
+            if "(" in line:
+                medium, line = line.split('(', 1)
+                az, line = re.split("[,\)]", line, 1)
+                if '"' in line or "'" in line:
+                    line = line.lstrip(": ")
+                    title,line = line.split (",",1)
+                else:
+                    title = ""
+                line = line.lstrip (", ")
+                iffer = line.split(",") [0]
+                #temp, line = re.split('[\'"]', line, 1)
+                    #titel, line = re.split('[\'"]', line, 1)
+                ruegenliste.append((medium.strip(),az,title,iffer))
+    return ruegenliste
+ruegen = getruegen()
+print (ruegen)
