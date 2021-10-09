09c3be01000c703e7c89658064dcf8af56662a23
alpakathrowaway <games.vincent@gmx.de>
2021-10-09 22:45:00 +0200

    implemented /update api call

--- go-scraper/lib/api.go
+++ go-scraper/lib/api.go
@@ -2,6 +2,8 @@
 package goscraper_lib
 
 import (
+	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -13,6 +15,18 @@ type API struct {
 	server string
 	port   int
 	client *http.Client
+}
+
+// this structs are responsible for the update JSON send to the API
+type updateJSON struct {
+	Domain updateJSONnested `json:"domain"`
+}
+
+type updateJSONnested struct {
+	Domain  string   `json:"domain"`
+	Links   []string `json:"links"`
+	Network bool     `json:"network"`
+	Updated int      `json:"last_updated"`
 }
 
 // InitAPI initialises and returns a new API struct
@@ -41,4 +55,38 @@ func (api *API) GetSpecs() string {
 	}
 
 	return string(b)
+}
+
+// PostUpdate runs the /update api call with a POST request
+func (api *API) PostUpdate(domain string, links []string, network bool, updated int) string {
+	str := updateJSON{
+		updateJSONnested{
+			Domain:  domain,
+			Links:   links,
+			Network: network,
+			Updated: updated,
+		},
+	}
+
+	js, err := json.Marshal(str)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	/*
+		res, err := api.client.Post(api.server+":"+strconv.Itoa(api.port), "application/json", bytes.NewBuffer(js))
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer res.Body.Close()
+
+		b, err := io.ReadAll(res.Body)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+	*/
+	fmt.Println(string(js))
+	return ""
 }
