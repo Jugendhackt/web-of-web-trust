6c899d1f5e1aaf6f5e264d6def8078488358d281
alpakathrowaway <games.vincent@gmx.de>
2021-10-09 22:48:30 +0200

    added api path

--- go-scraper/lib/api.go
+++ go-scraper/lib/api.go
@@ -74,7 +74,7 @@ func (api *API) PostUpdate(domain string, links []string, network bool, updated 
 	if err != nil {
 		log.Fatal(err)
 	}
-	res, err := api.client.Post(api.server+":"+strconv.Itoa(api.port), "application/json", bytes.NewBuffer(js))
+	res, err := api.client.Post(api.server+":"+strconv.Itoa(api.port)+"/update", "application/json", bytes.NewBuffer(js))
 
 	if err != nil {
 		log.Fatal(err)
