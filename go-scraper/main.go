6dbe82f7392a7d39712c7921cbc7e0d78df51fc7
alpakathrowaway <games.vincent@gmx.de>
2021-10-10 09:27:10 +0200

    added scraper main and a config file

--- go-scraper/main.go
+++ go-scraper/main.go
@@ -1,11 +1,46 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+
 	scrab "go-scraper/lib"
 )
 
+type Configjs struct {
+	APIconf APIjs    `json:"api"`
+	Netgood []string `json:"netgood"`
+	Netbad  []string `json:"netbad"`
+}
+
+type APIjs struct {
+	Server string `json:"server"`
+	Port   int    `json:"port"`
+}
+
 func main() {
-	api := scrab.InitApi("localhost", 8080)
-	links := []string{"faz.net", "ard.de"}
-	api.PostUpdate("bild.de", links, false, 1337)
+	conf, err := ioutil.ReadFile("./config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	confjs := Configjs{}
+
+	err = json.Unmarshal(conf, &confjs)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	api := scrab.InitApi(confjs.APIconf.Server, confjs.APIconf.Port)
+
+	for _, url := range confjs.Netgood {
+		links := scrab.Scraper(url)
+		api.PostUpdate(url, links, true, 0)
+	}
+
+	for _, url := range confjs.Netbad {
+		links := scrab.Scraper(url)
+		api.PostUpdate(url, links, false, 0)
+	}
 }
